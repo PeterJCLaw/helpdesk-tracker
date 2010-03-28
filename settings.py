967a7657045ba00f21ea6e879fb3f8e361f5035f
sam <sam@ninjacow.xen.prgmr.com>
2010-03-28 20:55:37 +0000

    made settings.py relative

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 # Django settings for tracker project.
 
+import os.path #so that it's portable
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -10,7 +12,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/var/tracker/db.sqlite'             # Or path to database file if using sqlite3.
+DATABASE_NAME = os.path.abspath('db.sqlite')             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -69,7 +71,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/var/tracker/templates'
+    os.path.abspath('templates')
 )
 
 INSTALLED_APPS = (
