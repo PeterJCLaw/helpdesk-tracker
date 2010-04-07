f510a605a4b85a76b6664787cd7e1f98302b6e9d
Peter Law <PeterJCLaw@gmail.com>
2010-04-07 21:23:05 +0100

    Add a static_url tag and a static filter that give the url to the static content

--- track/templatetags/base_url.py
+++ track/templatetags/base_url.py
@@ -25,3 +25,27 @@ class BaseUrl(template.Node):
     def render(self, c):
         return settings.BASE_URL
 
+@register.filter
+def static(file):
+    '''
+    Concatenate root of path to static files with the given file.
+    This is BASE_URL + MEDIA_URL + file.
+    '''
+
+    return settings.BASE_URL + settings.MEDIA_URL + file
+
+@register.tag
+def static_url(parser, token):
+    '''
+    Return root of path to static files.
+    This is BASE_URL + MEDIA_URL.
+    '''
+
+    return StaticUrl()
+
+class StaticUrl(template.Node):
+    def __init__(self):
+        pass
+    def render(self, c):
+        return settings.BASE_URL + settings.MEDIA_URL
+
