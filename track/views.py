6f7e678b6c7ac17111412769d9285cdeef9d70b5
sam <sam@ninjacow.xen.prgmr.com>
2010-04-14 18:57:56 +0000

    add timestamping to initial message

--- track/views.py
+++ track/views.py
@@ -40,7 +40,7 @@ def update_issue(request):
         i = issue.objects.get(id = issid)
         #i.ongoingNotes = od
 	if (od != ""):
-		i.initialDesc += "\n" + datetime.datetime.now().strftime("[%H:%M:%S]: ")   + od
+		i.initialDesc += "\n" + datetime.datetime.now().strftime("[%H:%M:%S]: ") + od
 	i.ongoingNotes = ""
         i.status = int(statusupdate)
 	if (mentor != i.assignedTo):
@@ -63,7 +63,7 @@ def create_issue(request):
     if 1:
     #try:
         teamnumber = request.POST['tn']
-        longdesc = request.POST['longdesc']
+        longdesc = datetime.datetime.now().strftime("[%H:%M:%S]: ")+request.POST['longdesc']
         shortdesc = request.POST['shortdesc']
         i = issue()
         i.team = teamnumber
@@ -88,9 +88,12 @@ def allissues(request):
 
 
 def get_issue_json(request, issueid):
+    #start = time.time()
     if(1):
         iss = issue.objects.get(id=int(issueid))
         json = '{"description":"%s", "assigned":"%s", "status":%d, "touched":"%s"}' % (iss.initialDesc, iss.assignedTo, iss.status, iss.updated.strftime("%H:%M:%S"))
         json = json.replace("\n", "\\n")
+	#end = time.time() - start
+	#json = json.replace("}", ', "time":"%d"}' % end)
         return HttpResponse(json)
     
