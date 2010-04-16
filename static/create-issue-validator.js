e534bbe6aae4d424b9144d5b5c26cbf52b4a4271
Peter Law <PeterJCLaw@gmail.com>
2010-04-16 16:20:06 +0000

    add issue creation form validation

---
+++ static/create-issue-validator.js
@@ -0,0 +1,20 @@
+function validate(FORM) {
+	var fields = {};
+
+	fields['Team Name'] = FORM.tn;
+	fields['Short Description'] = FORM.shortdesc;
+	fields['Long Description'] = FORM.longdesc;
+
+	for( label in fields ) {
+		input = fields[label];
+		// null strings and spaces only strings not allowed
+		if(/(^$)|(^\s+$)/.test(input.value)) {
+			input.focus();
+			alert("Please enter a "+label+".");
+			return false;
+		}
+	}
+
+	return true;
+}
+
